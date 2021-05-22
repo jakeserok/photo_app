e106c5bd0f4b4e08f9cd4a49dbb7212aaa2de82e
jakeserok <jakeserok@gmail.com>
2021-05-21 21:33:30 -0400

    Navbar implemented

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   
   resources :images
   resources :profiles
-  devise_for :users
+  devise_for :users 
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
