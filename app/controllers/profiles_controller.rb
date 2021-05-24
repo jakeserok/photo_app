fe55e25408eeadf53ad591beaaadea6d6737414c
jakeserok <jakeserok@gmail.com>
2021-05-23 22:37:30 -0400

    Updated profile views to not give option to navigate to index

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -53,7 +53,7 @@ class ProfilesController < ApplicationController
   def destroy
     @profile.destroy
     respond_to do |format|
-      format.html { redirect_to profiles_url, notice: "Profile was successfully destroyed." }
+      format.html { redirect_to root_url, notice: "Profile was successfully destroyed." }
       format.json { head :no_content }
     end
   end
